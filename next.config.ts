5de4eb326493301d847b190f6cfdf376cfb4a22b
najimhaq <mdnajimulhaque@gmail.com>
2026-08-03 17:00:57 +0900

    config file warning solve

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
     ],
   },
 };
